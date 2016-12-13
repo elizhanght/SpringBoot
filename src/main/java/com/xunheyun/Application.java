0ab366f40e8036d7329089c56d080654bd535955
houshuai <229828946@162.com>
2016-12-14 00:03:45 +0800

    init

---
+++ src/main/java/com/xunheyun/Application.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.xunheyun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author HAITAO
+ */
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/xunheyun/controller/LoginController.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.xunheyun.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author HAITAO
+ */
+@RequestMapping(value="/user")
+public class LoginController {
+
+
+}
